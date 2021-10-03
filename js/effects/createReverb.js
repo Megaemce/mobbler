d45e210caf4500a2fd2e55cd80affced7d5d5614
Megaemce <1651451+Megaemce@users.noreply.github.com>
2021-10-03 13:05:10 +0200

    Reverb parameter renamed

--- js/effects/createReverb.js
+++ js/effects/createReverb.js
@@ -1,47 +1,47 @@
 import Module from "../classes/Module.js";
 import { audioContext } from "../main.js";
 
-export default function createReverb(event, initalDelay, initalLevel, initalBufferName) {
+export default function createReverb(event, initalDryness, initalWetness, initalBufferName) {
     let irNames = Object.keys(audioContext.nameIRBuffer);
 
-    const delay = initalDelay || 0.5
-    const level = initalLevel || 1
-    const bufferName = initalBufferName || irNames[1]
-    
+    const dryness = initalDryness || 0.5;
+    const wetness = initalWetness || 1;
+    const bufferName = initalBufferName || irNames[1];
+
     const drynessInfo = "Loudness of signal without any signal processing";
-    const levelInfo = "Loudness of signal with full amount of an effect";
+    const wetnessInfo = "Loudness of signal with full amount of an effect";
 
     let module = new Module("reverb", true, false, false, irNames);
 
-    module.createSlider("dryness", delay, 0, 5, 0.1, "sec", false, drynessInfo);
-    module.createSlider("level", level, 0, 5, 0.1, "", false, levelInfo);
+    module.createSlider("dryness", dryness, 0, 5, 0.1, "", false, drynessInfo);
+    module.createSlider("wetness", wetness, 0, 5, 0.1, "", false, wetnessInfo);
 
     module.audioNodes = {
         inputNode: { audioNode: audioContext.createGain() },
         outputNode: { audioNode: audioContext.createGain() },
         convolerNode: { audioNode: audioContext.createConvolver() },
-        levelNode: { audioNode: audioContext.createGain() },
-        delayNode: { audioNode: audioContext.createGain() },
+        wetnessNode: { audioNode: audioContext.createGain() },
+        drynessNode: { audioNode: audioContext.createGain() },
         dryness: (value) => {
-            module.audioNodes.delayNode.audioNode.gain.value = value;
+            module.audioNodes.drynessNode.audioNode.gain.value = value;
         },
-        level: (value) => {
-            module.audioNodes.levelNode.audioNode.gain.value = value;
+        wetness: (value) => {
+            module.audioNodes.wetnessNode.audioNode.gain.value = value;
         },
     };
 
     module.audioNodes.inputNode.audioNode.connect(module.audioNodes.convolerNode.audioNode);
-    module.audioNodes.inputNode.audioNode.connect(module.audioNodes.delayNode.audioNode);
-    module.audioNodes.convolerNode.audioNode.connect(module.audioNodes.levelNode.audioNode);
-    module.audioNodes.levelNode.audioNode.connect(module.audioNodes.outputNode.audioNode);
-    module.audioNodes.delayNode.audioNode.connect(module.audioNodes.outputNode.audioNode);
+    module.audioNodes.inputNode.audioNode.connect(module.audioNodes.drynessNode.audioNode);
+    module.audioNodes.convolerNode.audioNode.connect(module.audioNodes.wetnessNode.audioNode);
+    module.audioNodes.wetnessNode.audioNode.connect(module.audioNodes.outputNode.audioNode);
+    module.audioNodes.drynessNode.audioNode.connect(module.audioNodes.outputNode.audioNode);
 
     module.audioNodes.input = module.audioNodes.inputNode.audioNode;
     module.audioNodes.output = module.audioNodes.outputNode.audioNode;
 
     module.audioNodes.convolerNode.audioNode.buffer = audioContext.nameIRBuffer[bufferName];
-    module.audioNodes.delayNode.audioNode.gain.value = delay;
-    module.audioNodes.levelNode.audioNode.gain.value = level;
+    module.audioNodes.drynessNode.audioNode.gain.value = dryness;
+    module.audioNodes.wetnessNode.audioNode.gain.value = wetness;
 
     module.content.options.select.onchange = function () {
         module.audioNodes.convolerNode.audioNode.buffer = audioContext.nameIRBuffer[this.value];
