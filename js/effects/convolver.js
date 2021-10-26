c24ddfff2edf95c13a97a23db15599a64f80ba4a
Megaemce <1651451+Megaemce@users.noreply.github.com>
2021-10-26 20:32:18 +0200

    Switch inital argument and variables names in convoler.js

--- js/effects/convolver.js
+++ js/effects/convolver.js
@@ -1,17 +1,17 @@
 import Module from "../classes/Module.js";
 import { audioContext } from "../main.js";
 
-export default function convolver(event, bufferName, normalizer) {
-    const initalBufferName = bufferName || "IR_theater.wav";
-    const initalNormalizer = normalizer || false;
+export default function convolver(event, initalBufferName, initalNormalizer) {
+    const bufferName = initalBufferName || "IR_theater.wav";
+    const normalizer = initalNormalizer || false;
     const irNames = Object.keys(audioContext.nameIRBuffer);
 
     let module = new Module("convolver", true, false, true, irNames);
 
     // set audioNode with inital values
     module.audioNode = audioContext.createConvolver();
-    module.audioNode.buffer = audioContext.nameIRBuffer[initalBufferName];
-    module.audioNode.normalize = initalNormalizer;
+    module.audioNode.buffer = audioContext.nameIRBuffer[bufferName];
+    module.audioNode.normalize = normalizer;
 
     module.content.options.select.onchange = function () {
         module.audioNode.buffer = audioContext.nameIRBuffer[this.value];
