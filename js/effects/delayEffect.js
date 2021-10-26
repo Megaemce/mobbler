e3baa7d43c6aa05975073a5c9a0e4b3f2518c2bb
Megaemce <1651451+Megaemce@users.noreply.github.com>
2021-10-26 20:35:14 +0200

    Switch inital argument and variables names in delayEffect.js

--- js/effects/delayEffect.js
+++ js/effects/delayEffect.js
@@ -1,19 +1,19 @@
 import Module from "../classes/Module.js";
 import { audioContext } from "../main.js";
 
-export default function delayEffect(event, wetness, delay, feedback) {
-    const initalDelay = delay || 0.1;
-    const initalWetness = wetness || 0.3;
-    const initalFeedback = feedback || 0.8;
+export default function delayEffect(event, initalWetness, initalDelay, initalFeedback) {
+    const delay = initalDelay || 0.1;
+    const wetness = initalWetness || 0.3;
+    const feedback = initalFeedback || 0.8;
     const wetnessInfo = "Loudness of signal with full amount of an effect";
     const delayInfo = "Number of seconds from input signal to be storage and play back";
     const feedbackInfo = "The return of a portion of the output signal back into delay loop";
 
     let module = new Module("delay effect", true, false, false, undefined);
 
-    module.createSlider("wetness", initalWetness, 0, 5, 0.1, "", false, wetnessInfo);
-    module.createSlider("delay time", initalDelay, 0, 1, 0.1, "sec", false, delayInfo);
-    module.createSlider("feedback", initalFeedback, 0, 1, 0.1, "sec", false, feedbackInfo);
+    module.createSlider("wetness", wetness, 0, 5, 0.1, "", false, wetnessInfo);
+    module.createSlider("delay time", delay, 0, 1, 0.1, "sec", false, delayInfo);
+    module.createSlider("feedback", feedback, 0, 1, 0.1, "sec", false, feedbackInfo);
 
     module.audioNodes = {
         inputNode: { audioNode: audioContext.createGain() },
@@ -42,9 +42,9 @@ export default function delayEffect(event, wetness, delay, feedback) {
     module.audioNodes.input = module.audioNodes.inputNode.audioNode;
     module.audioNodes.output = module.audioNodes.outputNode.audioNode;
 
-    module.audioNodes.wetNode.audioNode.gain.value = initalWetness;
-    module.audioNodes.delayNode.audioNode.delayTime.value = initalDelay;
-    module.audioNodes.durationNode.audioNode.gain.value = initalFeedback;
+    module.audioNodes.wetNode.audioNode.gain.value = wetness;
+    module.audioNodes.delayNode.audioNode.delayTime.value = delay;
+    module.audioNodes.durationNode.audioNode.gain.value = feedback;
 
     // add inital cable when structure is fully build - getBoundingClientRect related
     module.addInitalCable();
