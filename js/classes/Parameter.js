86e94a92da0e87667824c2c930339ecf27ef54d0
Megaemce <1651451+Megaemce@users.noreply.github.com>
2021-12-16 21:39:59 +0100

    Cleanup Parameter class

--- js/classes/Parameter.js
+++ js/classes/Parameter.js
@@ -1,9 +1,6 @@
 export default class Parameter {
     constructor(value, valueSetFunction) {
         this._value = value || 0;
-        this._eventIndex = 0;
-        this._prevGotTime = 0;
-        this.events = [];
         this.valueSetFunction = valueSetFunction;
     }
     get value() {
